d5c8b700596b864dcb90643c53e551b5f67a6057
adwitafernanda <adwitafernanda4@gmail.com>
2023-06-22 11:11:58 +0700

    jika FRONT_position > REAR_position, iterasi dri FRONT hingga akhir array

--- circularqueue/circularqueue.cpp
+++ circularqueue/circularqueue.cpp
@@ -78,6 +78,18 @@ public:
 				cout << queue_array[FRONT_position] << "  ";
 				FRONT_position++;
 			}
+			cout << endl;
+		}
+		else {
+			//jika FRONT_position > REAR_position, iterasi dri FRONT hingga akhir array
+			while (FRONT_position <= max - 1) {
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+			}
+
+			FRONT_position = 0;
+
+			//iterasi dari
 		}
 	}
 
