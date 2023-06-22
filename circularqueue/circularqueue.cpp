828c76f6ce694332205318e23203c3b1bb504f60
adwitafernanda <adwitafernanda4@gmail.com>
2023-06-22 10:35:57 +0700

    Add project files.

---
+++ circularqueue/circularqueue.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Queue {
+	int FRONT, REAR, max = 5;
+	int queue_array[5];
+
+public:
+	Queue() {
+		FRONT = -1;
+		REAR = -1;
+	}
+
+	void insert() {
+
+	}
+};
