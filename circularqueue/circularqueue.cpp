01dba03b2ff96ef52ae5b5c415cf52ac6668d532
adwitafernanda <adwitafernanda4@gmail.com>
2023-06-22 11:01:25 +0700

    cek apakah antrian kosong

--- circularqueue/circularqueue.cpp
+++ circularqueue/circularqueue.cpp
@@ -60,7 +60,18 @@ public:
 		}
 	}
 
-	void display()
+	void display() {
+		int FRONT_position = FRONT;
+		int REAR_position = REAR;
+
+		//cek apakah antrian kosong
+		if (FRONT == -1) {
+			cout << "queue is empty\n";
+			return;
+		}
+
+		cout << "\nElement in the queue are: \n";
+	}
 
 
 
